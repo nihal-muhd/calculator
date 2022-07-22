68ab27fd4be31943e2771b6cb5cc8ef9e270f8e2
Nihal Muhammed <nihalmuhammed7025@gmail.com>
2022-07-22 20:41:32 +0530

    js calculator

---
+++ script.js
@@ -0,0 +1,8 @@
+function buttonclick(val)
+{
+    document.getElementById("screen").value=document.getElementById("screen").value+val
+}
+function clearDisplay()
+{
+    document.getElementById("screen").value=""
+}
